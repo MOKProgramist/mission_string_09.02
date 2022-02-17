facb651769465bc522270c6511a7440e54cf4a3d
MOKProgramist <is24romanovar@artcollege.ru>
2022-02-17 22:27:56 +0300

    Добавьте файлы проекта.

---
+++ 3/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задание: вывести слова начинающиеся с прописной буквы (А)
+
+            Console.WriteLine("Введите предложение: ");
+            // уюираем из строки эти символы и записываем в массив. пропускаем из записи пробелы, чтобы не было проблем дальше
+            string[] textArray = Console.ReadLine().Split(new char[] { ' ', ',', '.', '!', '?', '-' }, StringSplitOptions.RemoveEmptyEntries);
+
+            // создаем массив по словам отдельным
+            // string[] msv = textArray.Split(new char[] { ' ', ',', '.', '!', '?', '-' });
+            Console.WriteLine("\nВот что я нашел: ");
+            
+            for(int i = 0; i < textArray.Length; i++)
+            {
+                char item = textArray[i][0] ;
+                // проверяем прописную букву
+                // Console.WriteLine(item);
+                if (Char.IsUpper(item)) Console.WriteLine(textArray[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using static System.Console;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Удалить из сообщения все слова, которые заканчиваются на заданный символ.
+            WriteLine("Введите предложение: ");
+            // уюираем из строки эти символы и записываем в массив. пропускаем из записи пробелы, чтобы не было проблем дальше
+            string[] textArray = Console.ReadLine().Split(' ', ',', '.', '-', '!', '?');
+
+            Write("Введите символ, который будет находить слова (которые содержат его на конце) и удалять: ");
+            char removeChar = char.Parse(ReadLine());
+            // textArray = textArray.Where(p => p.EndsWith(removeChar)).ToArray();
+
+            Console.WriteLine("\nЭти слова были удалены: ");
+            foreach(string text in textArray)
+            {
+                if(text.EndsWith(removeChar)) WriteLine(text);
+            }
+
+            Read();
+        }
+    }
+}
